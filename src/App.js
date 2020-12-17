70785dd8fa0c2400c0d31a49ce72d514988ed8f0
unknown <vibinvb1301@gmail.com>
2020-12-17 17:39:38 +0530

    adds logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { Layout } from 'antd';
-import './App.css';
 import PageElement from "./scenes/components/PageElement";
 
-const { Header, Footer, Sider, Content } = Layout;
 const style = { background: '#001529', padding: '40px 0', color: '#fff' };
 
 class App extends React.Component {
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './scenes/components/styles/style.css';
 import reportWebVitals from './reportWebVitals';
-import PageElement from './scenes/components/PageElement';
 
 ReactDOM.render(
   <React.StrictMode>
